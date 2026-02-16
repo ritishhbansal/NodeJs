662c07ee6c780fd9d4b283336153a746f3422f32
Ritish Bansal <ritishbansal2006@gmail.com>
2026-02-16 22:52:26 +0530

    Add files via upload

---
+++ node1.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+function requestListener(req, res){
+  console.log(req); 
+    process.exit();//stop event loop
+}
+
+const server = http.createServer(requestListener);
+
+// server.listen(3030);//this number is port number
+
+const PORT = 3030;
+server.listen(PORT, () => {
+  console.log('Server running at https://localhost:${PORT}');
+});
